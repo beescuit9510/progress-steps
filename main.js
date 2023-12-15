ab96d5d2648114366c2a27e2efd21d0fa942b35c
beescuit <pengsoon20@gmail.com>
2023-12-16 06:15:34 +0900

    fix js script loading problem with tag defer

--- main.js
+++ main.js
@@ -1,37 +1,31 @@
-$(document).ready(() => {
-  const steps = document.querySelectorAll('.progress-bar__step')
-  const next = document.querySelector('.next')
-  const prev = document.querySelector('.prev')
-  const progressbar = document.querySelector('.progress-bar__line')
+const steps = document.querySelectorAll('.progress-bar__step')
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+const progressbar = document.querySelector('.progress-bar__line')
 
-  const lastStep = steps.length
-  let currentStep = 0
+const lastStep = steps.length
+let currentStep = 0
 
-  next.addEventListener('click', () => {
-    progressbar.style.width = `${
-      (100 / (steps.length - 1)) * (currentStep + 1)
-    }%`
-    currentStep++
+next.addEventListener('click', () => {
+  progressbar.style.width = `${(100 / (steps.length - 1)) * (currentStep + 1)}%`
+  currentStep++
 
-    steps[currentStep].classList.add('progress-bar__step--active')
+  steps[currentStep].classList.add('progress-bar__step--active')
+  prev.disabled = false
+  if (currentStep === lastStep - 1) {
+    next.disabled = true
     prev.disabled = false
-    if (currentStep === lastStep - 1) {
-      next.disabled = true
-      prev.disabled = false
-    }
-  })
+  }
+})
 
-  prev.addEventListener('click', () => {
-    steps[currentStep].classList.remove('progress-bar__step--active')
+prev.addEventListener('click', () => {
+  steps[currentStep].classList.remove('progress-bar__step--active')
+  next.disabled = false
+  if (currentStep === 1) {
     next.disabled = false
-    if (currentStep === 1) {
-      next.disabled = false
-      prev.disabled = true
-    }
+    prev.disabled = true
+  }
 
-    progressbar.style.width = `${
-      (100 / (steps.length - 1)) * (currentStep - 1)
-    }%`
-    currentStep--
-  })
+  progressbar.style.width = `${(100 / (steps.length - 1)) * (currentStep - 1)}%`
+  currentStep--
 })
