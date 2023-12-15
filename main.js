8a904804d2d184b70a9db80c67bcf3a31e79a573
beescuit <pengsoon20@gmail.com>
2023-12-16 06:11:34 +0900

    fix github page js script loading problem

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
-const steps = document.querySelectorAll('.progress-bar__step')
-const next = document.querySelector('.next')
-const prev = document.querySelector('.prev')
-const progressbar = document.querySelector('.progress-bar__line')
+$(document).ready(() => {
+  const steps = document.querySelectorAll('.progress-bar__step')
+  const next = document.querySelector('.next')
+  const prev = document.querySelector('.prev')
+  const progressbar = document.querySelector('.progress-bar__line')
 
-const lastStep = steps.length
-let currentStep = 0
+  const lastStep = steps.length
+  let currentStep = 0
 
-next.addEventListener('click', () => {
-  progressbar.style.width = `${(100 / (steps.length - 1)) * (currentStep + 1)}%`
-  currentStep++
+  next.addEventListener('click', () => {
+    progressbar.style.width = `${
+      (100 / (steps.length - 1)) * (currentStep + 1)
+    }%`
+    currentStep++
 
-  steps[currentStep].classList.add('progress-bar__step--active')
-  prev.disabled = false
-  if (currentStep === lastStep - 1) {
-    next.disabled = true
+    steps[currentStep].classList.add('progress-bar__step--active')
     prev.disabled = false
-  }
-})
+    if (currentStep === lastStep - 1) {
+      next.disabled = true
+      prev.disabled = false
+    }
+  })
 
-prev.addEventListener('click', () => {
-  steps[currentStep].classList.remove('progress-bar__step--active')
-  next.disabled = false
-  if (currentStep === 1) {
+  prev.addEventListener('click', () => {
+    steps[currentStep].classList.remove('progress-bar__step--active')
     next.disabled = false
-    prev.disabled = true
-  }
+    if (currentStep === 1) {
+      next.disabled = false
+      prev.disabled = true
+    }
 
-  progressbar.style.width = `${(100 / (steps.length - 1)) * (currentStep - 1)}%`
-  currentStep--
+    progressbar.style.width = `${
+      (100 / (steps.length - 1)) * (currentStep - 1)
+    }%`
+    currentStep--
+  })
 })
